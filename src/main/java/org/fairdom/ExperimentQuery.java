3321c3a0e3efc18b0205257df0f7cf2181eae1a4
quyen <thucquyendn@gmail.com>
2015-02-13 16:22:58 +0100

    get sample for a seek study id

--- src/main/java/org/fairdom/ExperimentQuery.java
+++ src/main/java/org/fairdom/ExperimentQuery.java
@@ -2,8 +2,11 @@ package org.fairdom;
 
 import ch.ethz.sis.openbis.generic.shared.api.v3.IApplicationServerApi;
 import ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.experiment.Experiment;
+import ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.sample.Sample;
 import ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.experiment.ExperimentFetchOptions;
+import ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.sample.SampleFetchOptions;
 import ch.ethz.sis.openbis.generic.shared.api.v3.dto.search.ExperimentSearchCriterion;
+import ch.ethz.sis.openbis.generic.shared.api.v3.dto.search.SampleSearchCriterion;
 import ch.systemsx.cisd.common.spring.HttpInvokerUtils;
 import ch.systemsx.cisd.common.ssl.SslCertificateHelper;
 
@@ -17,6 +20,7 @@ public class ExperimentQuery {
     public static void main(String[] args){
         ExperimentQuery query = new ExperimentQuery();
         List <Experiment> experiments = query.experiment("Study_1");
+        List <Sample> samples = query.sample("Study_1");
 
         System.out.println("Result:");
 
@@ -28,9 +32,11 @@ public class ExperimentQuery {
 
     public List <Experiment> experiment(String SeekID){
         ExperimentSearchCriterion criterion = new ExperimentSearchCriterion();
-        criterion.withProperty("SEEK_STUDY_ID").thatContains(SeekID);
+        criterion.withProperty("SEEK_STUDY_ID").thatEquals(SeekID);
+
         ExperimentFetchOptions options = new ExperimentFetchOptions();
         options.withProperties();
+        options.withProject();
 
         Authentication au = new Authentication("https://openbis-testing.fair-dom.org/openbis", "api-user", "api-user");
         IApplicationServerApi api = au.api();
@@ -39,4 +45,21 @@ public class ExperimentQuery {
         List <Experiment> experiments = api.searchExperiments(sessionToken, criterion, options);
         return experiments;
     }
+
+    public List <Sample> sample(String SeekID){
+        SampleSearchCriterion criterion = new SampleSearchCriterion();
+        criterion.withExperiment().withProperty("SEEK_STUDY_ID").thatEquals(SeekID);
+
+        SampleFetchOptions options = new SampleFetchOptions();
+        options.withProperties();
+        options.withExperiment().withProperties();
+
+        Authentication au = new Authentication("https://openbis-testing.fair-dom.org/openbis", "api-user", "api-user");
+        IApplicationServerApi api = au.api();
+        String sessionToken = au.authentication();
+
+        List <Sample> samples = api.searchSamples(sessionToken, criterion, options);
+        return samples;
+    }
+
 }
--- src/test/java/org/fairdom/ExperimentQueryTest.java
+++ src/test/java/org/fairdom/ExperimentQueryTest.java
@@ -1,6 +1,8 @@
 package org.fairdom;
 
 import ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.experiment.Experiment;
+import ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.sample.Sample;
+import ch.ethz.sis.openbis.generic.shared.api.v3.dto.id.sample.SampleIdentifier;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
@@ -35,5 +37,27 @@ public class ExperimentQueryTest {
         String seekStudyID = "someID";
         List<Experiment> experiments = query.experiment(seekStudyID);
         assertEquals(0, experiments.size());
+    }
+
+    @Test
+    public void getSampleWithSeekStudyID() throws Exception {
+        ExperimentQuery query = new ExperimentQuery();
+        String seekStudyID = "Study_1";
+        List<Sample> samples = query.sample(seekStudyID);
+        assertEquals(1, samples.size());
+        Sample sample = samples.get(0);
+        Experiment experiment = sample.getExperiment();
+
+        assertEquals(seekStudyID, experiment.getProperties().get("SEEK_STUDY_ID"));
+        SampleIdentifier identifier = new SampleIdentifier("/API_TEST/SAMPLE_1");
+        assertEquals(identifier, sample.getIdentifier());
+    }
+
+    @Test
+    public void getSampleWithSeekStudyIDNoResult() throws Exception {
+        ExperimentQuery query = new ExperimentQuery();
+        String seekStudyID = "SomeID";
+        List<Sample> samples = query.sample(seekStudyID);
+        assertEquals(0, samples.size());
     }
 }
