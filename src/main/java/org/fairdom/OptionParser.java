2990f0bb7e484e5b1a6de73bf584de5c9f232f78
quyen <thucquyendn@gmail.com>
2016-03-24 14:15:49 +0100

    missing import List

--- src/main/java/org/fairdom/OptionParser.java
+++ src/main/java/org/fairdom/OptionParser.java
@@ -1,5 +1,8 @@
 package org.fairdom;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
